41bae17951ecc122b65418963f9265ddd6692313
Jeffrey Kirchner <jkirchner@gmail.com>
2021-09-07 09:32:23 -0700

    add profile

---
+++ main/forms/profile_admin_form.py
@@ -0,0 +1,23 @@
+
+from django import forms
+from django.contrib.auth.models import User
+
+from main.models import Profile
+from main.models import Experiments
+
+class ProfileAdminForm(forms.ModelForm):   
+    # user = forms.ModelChoiceField(label = "User",
+    #                                      queryset=User.objects.all(),
+    #                                      widget=forms.Select(attrs={"v-on:change":"userChange"}))
+
+    organization = forms.CharField(label='Organization',
+                                   widget=forms.TextInput(attrs={"size":"125"}))    
+
+    experiments = forms.ModelMultipleChoiceField(label="Grant Access",
+                                                 required=False,
+                                                 queryset=Experiments.objects.all().order_by("name"),
+                                                 widget = forms.CheckboxSelectMultiple(attrs={}))                                                                                                                                                                                                                                                           
+
+    class Meta:
+        model=Profile   
+        exclude=['global_id','user']
