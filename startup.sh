2284eb3785bd81dcb7693d209e0a0b79210b5b47
Jeffrey Kirchner <jkirchner@gmail.com>
2024-05-10 08:43:24 -0700

    update startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
+echo "*** Startup.sh ***"
+echo "Run Migrations:"
 python manage.py migrate
-gunicorn --bind=0.0.0.0 --timeout 60 --max-requests 500 --max-requests-jitter 10  _esi_account_management.wsgi
+echo "Start gunicorn:"
+gunicorn --bind=0.0.0.0 --timeout 60 --max-requests 500 --max-requests-jitter 10  _esi_account_management.wsgi --workers=3
